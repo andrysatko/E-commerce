9710040931495a8b4a09f64a43589fa4835367b3
andrysatko <andrysatko@gmail.com>
2023-02-21 01:40:05 +0200

    added config module db module and installed some packges

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import {ConfigModule} from "@nestjs/config";
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 
 @Module({
-  imports: [UsersModule],
+  imports: [ConfigModule.forRoot({
+      envFilePath: `.${process.env.Env_Mode}.env`,
+  }),
+  TypeOrmModule.forRoot({
+      type:'postgres',
+      host: process.env.host,
+      port:Number( process.env.port),
+      username: process.env.type,
+      password: process.env.password,
+      database: process.env.database,
+      entities: [],
+      synchronize: true,
+  }),
+      UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
